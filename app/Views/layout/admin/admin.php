2b93bf9af967fff47984c285785d8579b0d6c421
riski-ardian <rardianr24@gmail.com>
2024-06-27 20:09:36 +0700

    Update admin.php

--- app/Views/layout/admin/admin.php
+++ app/Views/layout/admin/admin.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= base_url('/vendor/bootstrap/css/bootstrap.min.css') ?>">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= base_url('/vendor/fontawesome/css/all.min.css') ?>">
     <link rel="stylesheet" href="<?= base_url('/vendor/sweetalert/sweetalert2.min.css') ?>">
     <link rel="stylesheet" href="<?= base_url('/assets/css/style.css') ?>">
