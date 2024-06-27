c044829c176e8f9cff449d89ff856e9b3f534b86
riski-ardian <rardianr24@gmail.com>
2024-06-27 20:13:33 +0700

    Update user.php

--- app/Views/layout/user/user.php
+++ app/Views/layout/user/user.php
@@ -47,8 +47,8 @@
         </div>
     </div>
 
-    <script src="<?= base_url('/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
-    <script src="<?= base_url('/vendor/sweetalert/sweetalert2.all.min.js') ?>"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.12.0/dist/sweetalert2.all.min.js"></script>
     <script src="<?= base_url('/assets/js/script.js') ?>"></script>
     <?php if (session()->has('added')) : ?>
         <script>
